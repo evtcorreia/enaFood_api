e66ee5d277d2229f7a2a15136b02f6d615b98ab3
Everton Assis Correia <evtcorreia@gmail.com>
2022-12-05 00:30:07 -0300

    implementação criação de categorias

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -4,9 +4,23 @@ class CategoriaController{
 
     static listarCategorias = async (req, res)=>{
 
+        categorias.find((err, categorias)=>{
+            res.status(200).json(categorias)
+        })
+
     }
 
     static adicionaCategoria = async (req, res)=>{
+
+        const categoria = new categorias(req.body)
+
+        categoria.save((err)=>{
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao cadastrar categoria`})
+            }else{
+                res.status(201).send(categoria.toJSON())
+            }
+        })
 
     }
 
--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -5,7 +5,7 @@ const categoriaSchema = new mongoose.Schema(
     {
         id: {type: String},
         categoria: {type: String, required: true},
-        D_E_L_E_T: {type: String, requires:true}
+        D_E_L_E_T: {type: String, requires:false}
     }
 )
 
